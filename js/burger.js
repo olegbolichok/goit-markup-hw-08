c6d1ec008816025f4674e9d2f531a8a07ec4c106
Oleg Bolichok <olegbolichok@gmail.com>
2022-11-12 02:06:34 +0400

    adaptive images markuped

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,18 @@
 (() => {
     const menuBtnRef = document.querySelector('[data-menu-button]');
     const mobileMenuRef = document.querySelector('[data-menu]');
+    const closeModalBtn = document.querySelector('[data-menu-close]');
 
     menuBtnRef.addEventListener('click', () => {
         const expanded =
             menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-        menuBtnRef.classList.toggle('is-open');
         menuBtnRef.setAttribute('aria-expanded', !expanded);
-
+        menuBtnRef.classList.toggle('is-open');
+        mobileMenuRef.classList.toggle('is-open');
+    });
+    closeModalBtn.addEventListener('click', () => {
+        menuBtnRef.classList.toggle('is-open');
         mobileMenuRef.classList.toggle('is-open');
     });
 })();
